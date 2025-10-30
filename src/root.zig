f258bdf4456f7022dab07358a2bf1bf5224d81c4
M. Elijah Wangeman <maw4678@rit.edu>
2025-10-30 12:02:22 -0400

    i know how writing strings to stdout works now

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,7 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 
-pub fn bufferedPrint() !void {
+pub fn bufferedPrint(msg: []const u8) !void {
     // Stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
@@ -9,15 +9,7 @@ pub fn bufferedPrint() !void {
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("{s}", .{msg});
 
     try stdout.flush(); // Don't forget to flush!
-}
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
 }
